f64fd51f3e41c3ea0841532d7a6a855059b0a353
Roy <a0966750212@icloud.c0m>
2021-09-03 21:03:18 +0800

    add git comm

---
+++ classic02/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  classic02
+//
+//  Created by 暨大附中 on 2021/9/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
